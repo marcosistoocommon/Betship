9f519c82800ef2c5b7bb4c2a5eb95106b0da5db9
marcosistoocommon <maperu2004@gmail.com>
2026-07-19 20:19:06 +0200

    testing the waters

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)   
 
 @app.route('/')
 def hello():
     return "Hello, World!"
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        name = request.form['username']
+        return f"Hello {name}, POST request received"
+    return render_template('name.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
