1f9481cb103de66fa0f08ab0609931c3301340cc
marcosistoocommon <maperu2004@gmail.com>
2026-07-27 03:34:14 +0200

    fix dotenv

--- app.py
+++ app.py
@@ -12,6 +12,11 @@ from functools import wraps
 from flask import Flask, abort, flash, jsonify, redirect, render_template, request, session, url_for
 from werkzeug.security import check_password_hash, generate_password_hash
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 app = Flask(__name__)
 app.secret_key = os.environ.get("BETSHIP_SECRET_KEY")
 if not app.secret_key:
