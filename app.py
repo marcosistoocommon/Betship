5d307aefa04299eace4d7ddc1f754a187f8d3890
marcosistoocommon <maperu2004@gmail.com>
2026-07-27 03:42:14 +0200

    opened for testing

--- app.py
+++ app.py
@@ -974,4 +974,4 @@ init_db()
 
 
 if __name__ == '__main__':
-    app.run(debug=os.environ.get("FLASK_DEBUG") == "1")
+    app.run(debug=os.environ.get("FLASK_DEBUG") == "1", host="0.0.0.0", port=5000)
